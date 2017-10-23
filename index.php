d856701aea75ab5c699fb7512ee0ff360a4ee28e
bolino <moi@adriencarpentier.com>
2017-10-23 03:52:08 +0200

    clean version

--- index.php
+++ index.php
@@ -2,65 +2,35 @@
 <html lang="fr">
 
 	<head>
-
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<style type="text/css">
 			html, body {
 			    height: 100%;
 			    margin: 0;
 			}
-
-			#wrapper {
+			#animated-background-wrapper {
 				position:absolute;
 				top:0px;
 				left:0px;
 				overflow:hidden;
 				width:100%;
 				height:100%;
-			}
-			#a {
-				width: 50px;
-				height:50px;
-				background-color:red;
-				position:fixed;
-
-			}
-			#b{
-
-			}
-			#c{
-				width: 50px;
-				height: 50px;
-				background-image: url('images_color/dossier.png');
-				background-size: contain;
-				background-repeat: no-repeat;
-				position: fixed;
+				z-index: -9999;
 			}
 		</style>
-
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="config_bg.json"></script>
-		<script type="text/javascript" src='main.js'></script>
-
+		<script type="text/javascript" src="config_bg_test.json"></script>
+		<script type="text/javascript" src='animated_background.js'></script>
 	</head>
 	
 	<body>
 
-		<!-- <div id="a"></div> -->
-		<!-- <div id="b"></div> -->
-		<!-- <div id="c"></div> -->
-		<!-- <img id="i" src ="images_color/dossier.png" /> -->
-			ldqksflqdknv<br>
-			sqkjcqskjhfj<br>
-			ldqksflqdknv<br>
-			sqkjcqskjhfj<br>
-			
-			ldqksflqdknv<br>
-			sqkjcqskjhfj<br>
-		<div id="wrapper">
-
-		
+		<div class="content">
+			<p style="width:300px; color: white; background-color: black; margin: auto;">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
+
+		<div id="animated-background-wrapper"></div>
 
 	</body>
 
